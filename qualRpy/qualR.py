9ad56e176c0008328d1b93754466b6b79c811f85
lokamigauti <leonardo.kamigauti@usp.br>
2023-02-07 12:16:43 -0300

    Attribute file_name inside `if csv` for minor optimization

--- qualRpy/qualR.py
+++ qualRpy/qualR.py
@@ -86,49 +86,49 @@ def cetesb_retrieve(cetesb_login, cetesb_password,
         DataFrame with a column with date and parameter values.
 
     '''
-     
-  
+
+
     login_data = {
         'cetesb_login': cetesb_login,
         'cetesb_password': cetesb_password
     }
-    
+
     search_data = {
         'irede': 'A',
-        'dataInicialStr':start_date,
-        'dataFinalStr':end_date,
+        'dataInicialStr': start_date,
+        'dataFinalStr': end_date,
         'iTipoDado': 'P',
-        'estacaoVO.nestcaMonto':station,
-        'parametroVO.nparmt':parameter
+        'estacaoVO.nestcaMonto': station,
+        'parametroVO.nparmt': parameter
     }
-    
+
     with requests.Session() as s:
         url = "https://qualar.cetesb.sp.gov.br/qualar/autenticador"
         r = s.post(url, data=login_data)
         url2 = "https://qualar.cetesb.sp.gov.br/qualar/exportaDados.do?method=pesquisar"
         r = s.post(url2, data=search_data)
         soup = BeautifulSoup(r.content, 'lxml')
-        
+
     data = []
-    table = soup.find('table', attrs={'id':'tbl'})
+    table = soup.find('table', attrs={'id': 'tbl'})
     rows = table.find_all('tr')
     row_data = rows[2:]
     for row in row_data:
         cols = row.find_all('td')
         cols = [ele.text.strip() for ele in cols]
-        data.append([ele for ele in cols if ele])    
-        
+        data.append([ele for ele in cols if ele])
+
     dat = pd.DataFrame(data)
-           
+
     # Creating a complete df with all dates
     day1 = pd.to_datetime(start_date, format='%d/%m/%Y')
     day2 = pd.to_datetime(end_date, format='%d/%m/%Y') + dt.timedelta(days=1)
-    all_date = pd.DataFrame(index=pd.date_range(day1.strftime('%m/%d/%Y'), 
+    all_date = pd.DataFrame(index=pd.date_range(day1.strftime('%m/%d/%Y'),
                                                 day2.strftime('%m/%d/%Y'),
                                                 freq='H'))
     if len(dat) <= 1:
-        dat = pd.DataFrame(columns=['day', 'hour', 'name', 'pol_name', 'units', 'val'])        
-    else:    
+        dat = pd.DataFrame(columns=['day', 'hour', 'name', 'pol_name', 'units', 'val'])
+    else:
         dat = dat[[3, 4, 6, 7, 8, 9]]
         dat.columns = ['day', 'hour', 'name', 'pol_name', 'units', 'val']
         dat['date'] = dat.day + '_' + dat.hour
@@ -139,13 +139,9 @@ def cetesb_retrieve(cetesb_login, cetesb_password,
         # Changing val type from string/object to numeric
         dat['val'] = dat.val.str.replace(',', '.').astype(float)
 
-        # Filling empy dates
-        dat.set_index('date', inplace=True)
-       
-    
     dat_complete = all_date.join(dat)
-    file_name = str(parameter) + '_' + str(station) + '.csv'
     if csv:
+        file_name = str(parameter) + '_' + str(station) + '.csv'
         dat_complete.to_csv(file_name, index_label='date')
     else:
         return dat_complete
@@ -291,4 +287,34 @@ def cetesb_retrieve_met(cetesb_login, cetesb_password,
     else:
         return all_met_df
 
-    
+# def compact_cetesb_retrieve(cetesb_login, cetesb_password,
+#                     start_date, end_date,
+#                     parameter, station, csv=False):
+
+'''
+    Download a parameter for one Air Quality Station station 
+    from CETESB AQS network
+
+    Parameters
+    ----------
+    cetesb_login : str
+        cetesb qualar username.
+    cetesb_password : str
+        cetesb qualar username's password.
+    start_date : str
+        date to start download in %dd/%mm/%YYYY.
+    end_date : str
+        date to end download in %dd/%mm/%YYYY.
+    parameter : int
+        parameter code.
+    station : int
+        AQS code.
+    csv : Bool, optional
+        Export to csv file. The default is False.
+
+    Returns
+    -------
+    dat_complete : pandas DataFrame
+        DataFrame with a column with date and parameter values.
+
+    '''
