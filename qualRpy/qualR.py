bd6e3230f823f4accfcd6bd1f037ea41d5cb4a8e
lokamigauti <leonardo.kamigauti@usp.br>
2023-02-09 09:01:14 -0300

    Make cetesb_retrieve_robust

--- qualRpy/qualR.py
+++ qualRpy/qualR.py
@@ -1,9 +1,12 @@
+from typing import Tuple, List
+
 import requests
 import pkg_resources
 import pandas as pd
 import datetime as dt
 from bs4 import BeautifulSoup
 from itertools import product as iter_product
+import os
 
 from pandas import DataFrame
 
@@ -22,6 +25,7 @@ def cetesb_aqs():
     aqs_stream = pkg_resources.resource_stream(__name__, "data/cetesb_lat_lon.csv")
     return pd.read_csv(aqs_stream)
 
+
 def cetesb_param():
     '''
     Return a dataframe with parameters and their codes required to use
@@ -33,6 +37,7 @@ def cetesb_param():
     '''
     param_stream = pkg_resources.resource_stream(__name__, "data/cetesb_param.csv")
     return pd.read_csv(param_stream)
+
 
 def my_to_datetime(date_str):
     '''
@@ -57,12 +62,13 @@ def my_to_datetime(date_str):
 
     date_str = date_str[0:11] + '00' + date_str[13:]
     return pd.to_datetime(date_str, format='%d/%m/%Y_%H:%M') + \
-           dt.timedelta(days=1)
+        dt.timedelta(days=1)
 
 
 def cetesb_retrieve(cetesb_login: str, cetesb_password: str,
-                    start_date: str, end_date :str,
-                    parameter: int | list[int], station: int | list[int], csv: bool=False, all_dates: bool=True) -> pd.DataFrame | None:
+                    start_date: str, end_date: str,
+                    parameter: int | list[int], station: int | list[int], csv: bool = False,
+                    all_dates: bool = True) -> pd.DataFrame | None:
     """
     Download a parameter for one Air Quality Station station
     from CETESB AQS network. Passing lists of integers to parameter and/or stations
@@ -176,9 +182,137 @@ def cetesb_retrieve(cetesb_login: str, cetesb_password: str,
     return dat_complete
 
 
-def cetesb_retrieve_pol(cetesb_login, cetesb_password, 
-                       start_date, end_date, station, 
-                       csv=False, voc=False):
+def cetesb_retrieve_robust(cetesb_login: str, cetesb_password: str, start_date: str, end_date: str,
+                           parameter: int | list[int], station: int | list[int], save_path: str,
+                           max_iter: int = 100, overwrite: bool = False) -> tuple[
+    list[list[str | int | list[int]]], list[list[str | int | list[int]]]]:
+    """
+    Robust version of cetesb_retrieve for large datasets.
+    Download parameter(s) for Air Quality station(s)
+    from CETESB AQS network.
+    The is saved in a file each iteration.
+    Passing lists of integers to parameter and/or stations generate duplicated indexes.
+    This function do not return the final data, but save it as a csv.
+
+    Parameters
+    ----------
+    cetesb_login : str
+        cetesb qualar username.
+    cetesb_password : str
+        cetesb qualar username's password.
+    start_date : str
+        date to start download in %dd/%mm/%YYYY.
+    end_date : str
+        date to end download in %dd/%mm/%YYYY.
+    parameter : int or list of int
+        parameter code.
+    station : int or list of int
+        AQS code.
+    save_path : str
+        File path or directory. If a directory is passed, a 'data.csv' file is created.
+    max_iter : positive int
+        Max number of requests in case of failure.
+    overwrite : bool
+        Overwrite existing file. Default is False.
+
+    Returns
+    -------
+    report : tuple[
+    list[list[str | int | list[int]]], list[list[str | int | list[int]]]]
+        A tuple with lists of station_parameter combinations of successifull and unsuccessiful downloads.
+    """
+
+    # Assert types
+    assert isinstance(save_path, str), 'save_path must be a string'
+    path = os.path.abspath(save_path)
+    if os.path.isdir(path):
+        assert os.path.exists(save_path), 'Directory not found'
+        save_path = path + 'data.csv'
+    assert os.path.exists(os.path.basename(save_path)), 'Directory not found'
+    if not overwrite:
+        assert os.path.exists(save_path), 'File already exists'
+
+    if type(station) is not list:
+        station = [station]
+    if type(parameter) is not list:
+        parameter = [parameter]
+
+    # Make header
+    pd.DataFrame(columns=['day', 'hour', 'name', 'pol_name', 'units', 'val']).to_csv(save_path)
+
+    # Make scrappers` inputs
+    login_data = {
+        'cetesb_login': cetesb_login,
+        'cetesb_password': cetesb_password
+    }
+    search_data_list = []
+    search_data_proto = {
+        'irede': 'A',
+        'dataInicialStr': start_date,
+        'dataFinalStr': end_date,
+        'iTipoDado': 'P'
+    }
+    for station_number, parameter_number in iter_product(station, parameter):
+        search_data_station = search_data_proto.copy()
+        search_data_station['estacaoVO.nestcaMonto'] = station_number
+        search_data_station['parametroVO.nparmt'] = parameter_number
+        search_data_list.append(search_data_station)
+
+    def try_request(session, search):
+        for _ in range(1, max_iter):
+            request = session.post(url2, data=search)
+            if request is not None:
+                return request
+        return None
+
+    def clean_scrap(scrap_data):
+        data = []
+        table = scrap_data.find('table', attrs={'id': 'tbl'})
+        rows = table.find_all('tr')
+        row_data = rows[2:]
+        for row in row_data:
+            cols = row.find_all('td')
+            cols = [ele.text.strip() for ele in cols]
+            data.append([ele for ele in cols if ele])
+
+        dat = pd.DataFrame(data)
+
+        if len(dat) <= 1:
+            return None
+
+        dat = dat[[3, 4, 6, 7, 8, 9]]
+        dat.columns = ['day', 'hour', 'name', 'pol_name', 'units', 'val']
+        dat['date'] = dat.day + '_' + dat.hour
+        dat['date'] = dat.date.apply(my_to_datetime)
+        dat['val'] = dat.val.str.replace(',', '.').astype(float)
+
+        return dat
+
+    successes = []
+    failures = []
+    with requests.Session() as s:
+        url = "https://qualar.cetesb.sp.gov.br/qualar/autenticador"
+        _ = s.post(url, data=login_data)
+        url2 = "https://qualar.cetesb.sp.gov.br/qualar/exportaDados.do?method=pesquisar"
+        for search_data in search_data_list:
+            r = try_request(s, search_data)
+            if r is None:
+                failures.append([search_data['estacaoVO.nestcaMonto'], search_data['parametroVO.nparmt']])
+                continue
+            scrap = BeautifulSoup(r.content, 'lxml')
+            scrap = clean_scrap(scrap)
+            if scrap is None:
+                failures.append([search_data['estacaoVO.nestcaMonto'], search_data['parametroVO.nparmt']])
+                continue
+            scrap.to_csv(save_path, mode='a', header=False)
+            successes.append([search_data['estacaoVO.nestcaMonto'], search_data['parametroVO.nparmt']])
+
+    return successes, failures
+
+
+def cetesb_retrieve_pol(cetesb_login, cetesb_password,
+                        start_date, end_date, station,
+                        csv=False, voc=False):
     '''
     Download photochemical pollutants 
 
@@ -204,22 +338,22 @@ def cetesb_retrieve_pol(cetesb_login, cetesb_password,
         O3, NO, NO2, CO, PM10, PM2.5 columns.
 
     '''
-    o3 = cetesb_retrieve(cetesb_login, cetesb_password, 
+    o3 = cetesb_retrieve(cetesb_login, cetesb_password,
                          start_date, end_date, 63, station)
-    no = cetesb_retrieve(cetesb_login, cetesb_password, 
+    no = cetesb_retrieve(cetesb_login, cetesb_password,
                          start_date, end_date, 17, station)
-    no2 = cetesb_retrieve(cetesb_login, cetesb_password, 
+    no2 = cetesb_retrieve(cetesb_login, cetesb_password,
                           start_date, end_date, 15, station)
-    nox = cetesb_retrieve(cetesb_login, cetesb_password, 
+    nox = cetesb_retrieve(cetesb_login, cetesb_password,
                           start_date, end_date, 18, station)
-    co = cetesb_retrieve(cetesb_login, cetesb_password, 
+    co = cetesb_retrieve(cetesb_login, cetesb_password,
                          start_date, end_date, 16, station)
-    so2 = cetesb_retrieve(cetesb_login, cetesb_password, 
-                         start_date, end_date, 13, station)
-    pm10 = cetesb_retrieve(cetesb_login, cetesb_password, 
-                         start_date, end_date, 12, station)
-    pm25 = cetesb_retrieve(cetesb_login, cetesb_password, 
-                         start_date, end_date, 57, station)
+    so2 = cetesb_retrieve(cetesb_login, cetesb_password,
+                          start_date, end_date, 13, station)
+    pm10 = cetesb_retrieve(cetesb_login, cetesb_password,
+                           start_date, end_date, 12, station)
+    pm25 = cetesb_retrieve(cetesb_login, cetesb_password,
+                           start_date, end_date, 57, station)
 
     all_photo_df = pd.DataFrame({
         'o3': o3.val,
@@ -233,25 +367,25 @@ def cetesb_retrieve_pol(cetesb_login, cetesb_password,
     }, index=o3.index)
 
     if voc:
-        tol = cetesb_retrieve(cetesb_login, cetesb_password, 
+        tol = cetesb_retrieve(cetesb_login, cetesb_password,
                               start_date, end_date, 62, station)
-        ben = cetesb_retrieve(cetesb_login, cetesb_password, 
+        ben = cetesb_retrieve(cetesb_login, cetesb_password,
                               start_date, end_date, 61, station)
         all_photo_df["tol"] = tol.val
         all_photo_df["ben"] = ben.val
 
     all_photo_df.index = all_photo_df.index.tz_localize('America/Sao_Paulo')
-    
+
     if csv:
         all_photo_df.to_csv('all_pol_' + str(station) + '.csv',
                             index_label='date')
     else:
         return all_photo_df
 
-    
+
 def cetesb_retrieve_met(cetesb_login, cetesb_password,
-                        start_date, end_date, station, 
-                        in_k = False, rm_flag = True,
+                        start_date, end_date, station,
+                        in_k=False, rm_flag=True,
                         csv=False):
     '''
     Download meteorological parameters
@@ -282,36 +416,36 @@ def cetesb_retrieve_met(cetesb_login, cetesb_password,
         TC, RH, WS and WD columns.
 
     '''
-    tc = cetesb_retrieve(cetesb_login, cetesb_password, 
+    tc = cetesb_retrieve(cetesb_login, cetesb_password,
                          start_date, end_date, 25, station)
-    rh = cetesb_retrieve(cetesb_login, cetesb_password, 
+    rh = cetesb_retrieve(cetesb_login, cetesb_password,
                          start_date, end_date, 28, station)
-    ws = cetesb_retrieve(cetesb_login, cetesb_password, 
+    ws = cetesb_retrieve(cetesb_login, cetesb_password,
                          start_date, end_date, 24, station)
-    wd = cetesb_retrieve(cetesb_login, cetesb_password, 
+    wd = cetesb_retrieve(cetesb_login, cetesb_password,
                          start_date, end_date, 23, station)
     if in_k:
         K = 273.15
     else:
         K = 0
-    
+
     all_met_df = pd.DataFrame({
         't2': tc.val + K,
         'rh2': rh.val,
         'ws': ws.val,
         'wd': wd.val
     }, index=tc.index)
-    
+
     all_met_df.index = all_met_df.index.tz_localize('America/Sao_Paulo')
-    
+
     # Filtering 777 and 888 values
     if rm_flag:
         filter_flags = all_met_df['wd'] <= 360
         all_met_df['wd'].where(filter_flags, inplace=True)
-    
+
     # Export to csv
     if csv:
         all_met_df.to_csv('all_met_' + str(station) + '.csv',
-                            index_label='date')
+                          index_label='date')
     else:
         return all_met_df
